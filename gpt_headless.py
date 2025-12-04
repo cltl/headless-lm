a0c4eb98814230329e2744ea7f253249f804ca15
sarnoult <s.i.arnoult@vu.nl>
2025-12-04 14:18:03 +0100

    Prepare gpt training

--- gpt_headless.py
+++ gpt_headless.py
@@ -4,51 +4,58 @@ from engine.tasks.pretraining import GptHeadlessPretraining
 from engine.lit.lightning_module import TaskTrainer
 from transformers import AutoTokenizer, AutoConfig
 from pytorch_lightning.callbacks import ModelCheckpoint
-import psutil
+import logging
 import argparse
 import torch
 import time
 import json
 
-print("CPU count: ", psutil.cpu_count())
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+# print("CPU count: ", psutil.cpu_count())
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--config")
+parser.add_argument("-c", "--config")
+parser.add_argument("-j", "--job_config")
 parser.add_argument("--num_nodes")
 parser.add_argument("--global_bs")
 parser.add_argument("--gpu_bs")
 parser.add_argument("--dataset")
 parser.add_argument("--hf_tokenizer")
-
 parser.add_argument("--run_name")
 parser.add_argument("--hf_path")
 parser.add_argument("--accelerator", default="hf")
 parser.add_argument("--precision", default="16-mixed")
 parser.add_argument('--ckpt_path', nargs='?', const=None, type=str)
-
 parser.add_argument('--model_max_seq_len', default=2048)
 parser.add_argument('--saved_ckpt_path')
 parser.add_argument("--ckpt_every", default=10000)
 
 args = parser.parse_args()
 
-config = args.config
-num_nodes = int(args.num_nodes)
-ckpt_path = args.ckpt_path
-global_bs = int(args.global_bs)
-gpu_bs = int(args.gpu_bs)
-dataset = args.dataset
-hf_tokenizer = args.hf_tokenizer
+config = json.load(open(args.config, "rb"))
+model_max_seq_len = int(config.get("model_max_seq_len", args.model_max_seq_len)
 
-model_max_seq_len = int(args.model_max_seq_len)
+job_config = {}
+if args.job_config is not None:
+    job_config = json.load(open(args.job_config, "rb"))
 
-run_name = args.run_name
-hf_path = args.hf_path
-accelerator = args.accelerator
-precision = args.precision
-ckpt_every = args.ckpt_every
+num_gpus = int(job_config.get("num_gpus", args.num_gpus))
+num_workers = int(job_config.get("num_workers", args.num_workers))
+ckpt_path = job_config.get("ckpt_path", args.ckpt_path)
+accu_grad_batches = int(job_config.get(
+    "accu_grad_batches", args.accu_grad_batches))
+gpu_bs = int(job_config.get("gpu_bs", args.gpu_bs))
+dataset = job_config.get("dataset", args.dataset)
+hf_tokenizer = job_config.get("hf_tokenizer", args.hf_tokenizer)
+run_name = job_config.get("run_name", args.run_name)
+hf_path = job_config.get("hf_path", args.hf_path)
+accelerator = job_config.get("accelerator", args.accelerator)
+precision = job_config.get("precision", args.precision)
+ckpt_every = job_config.get("ckpt_every", args.ckpt_every)
+saved_ckpt_path = job_config.get("saved_ckpt_path", args.saved_ckpt_path)
 
-saved_ckpt_path = args.saved_ckpt_path
 
 if accelerator == "xformers":
   from engine.models.xformers.efficient_gpt_neox import GPTNeoXForCausalLM
@@ -62,23 +69,26 @@ else:
 if "A100" in torch.cuda.get_device_name():
   torch.set_float32_matmul_precision('high')
 
-gpus_by_node = torch.cuda.device_count()
+global_bs = accu_grad_batches * num_gpus * gpu_bs
+logger.info(
+    f"Global batch size is: {global_bs}, based on selected batch size, number of gpus and accumulation factor"
+)
 
-if ((gpus_by_node * num_nodes) % global_bs) == 0:
-  raise argparse.ArgumentError(f"Requested a batch size of {global_bs} on {gpu_bs}x{gpus_by_node} GPUs : not a multiple!")
-accu_grad_batches = global_bs // (gpus_by_node * num_nodes * gpu_bs)
-print(f"GPU BS: {gpu_bs}; Grad. accumulating factor: {accu_grad_batches}")
-
-
-datamodule = DataModule.from_datasets(dataset, train_batch_size=gpu_bs, infer_batch_size=gpu_bs,
-split_names=["train(:0.9999)", "train(0.9999:)"], from_disk=True, num_workers=0)
+datamodule = DataModule.from_datasets(
+    dataset,
+    train_batch_size=gpu_bs,
+    infer_batch_size=gpu_bs,
+    split_names=["train", "validation", "test"],
+    from_disk=True,
+    num_workers=num_workers,
+)
 
 tokenizer = AutoTokenizer.from_pretrained(hf_tokenizer)
 lm_config = AutoConfig.from_pretrained(hf_path)
 
 lm_config.max_position_embeddings = model_max_seq_len
 lm_model = GPTNeoXForCausalLM(lm_config)
-print(lm_model)
+logger.info(lm_model)
 
 
 task = GptHeadlessPretraining(
@@ -86,16 +96,17 @@ task = GptHeadlessPretraining(
 )
 
 version_name = run_name
-trainer = TaskTrainer(task, logger_args={"version": version_name})
-
+trainer = TaskTrainer(
+    task, logger="wandb", logger_args={"project": "gpt-headless", "name": version_name}
+)
 checkpoints = [
   ModelCheckpoint(every_n_train_steps=ckpt_every, dirpath=f'{saved_ckpt_path}/{version_name}', save_top_k=-1),
-  ModelCheckpoint(every_n_train_steps=1000, dirpath=f'{saved_ckpt_path}/{version_name}', save_top_k=1)
+  # ModelCheckpoint(every_n_train_steps=1000, dirpath=f'{saved_ckpt_path}/{version_name}', save_top_k=1)
 ]
 
 trainer.fit(
   datamodule,
-  num_nodes=num_nodes,
+  # num_nodes=num_nodes,
   precision=precision,
   accumulate_grad_batches=accu_grad_batches,
   callbacks=checkpoints,
---
+++ scripts/ft_gpt_headless.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --partition=gcn
+#SBATCH --cpus-per-task=8
+#SBATCH --gpus=1
+#SBATCH --time=04:00:00
+#SBATCH --output=logs/ft_gpt_headless.log
+
+wdir=$HOME/headless-lm
+
+# Loading modules
+module purge
+module load 2025
+module load Python/3.13.1-GCCcore-14.2.0
+
+# Prepare virtual environment
+# virtualenv $wdir/.venv --system-site-packages
+source $wdir/.venv/bin/activate
+
+cd $TMPDIR
+mkdir expdata
+cd expdata
+srun python $wdir/ft_gpt_headless.py -c $wdir/configs/ft_gpt_headless_test.json -j $wdir/configs/train_ft_gpt_headless_wikitext_bpe_test.json
+
+ls -l
+cd ..
+cp -r expdata $wdir
